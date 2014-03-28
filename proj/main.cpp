dd51ffed001240dab63bc18585cd352de5598c49
mellwa <kolachao.chen@gmail.com>
2014-03-28 14:46:37 -0400

    Initial Commit

---
+++ proj/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  proj
+//
+//  Created by Chao Chen on 2014-03-28.
+//  Copyright (c) 2014 Chao Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
